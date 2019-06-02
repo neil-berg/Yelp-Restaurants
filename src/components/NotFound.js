608cae4af96fb3bf57595f588e61cb6c3cf1ddd4
Neil Berg <neil.berg14@gmail.com>
2019-06-01 20:12:15 -0700

    NotFound icon

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { ReactComponent as NotFoundSVG } from '../assets/ui-icons/error-404.svg';
 
 const NotFound = ({ handleOutsideClick }) => {
   return (
-    <div onClick={handleOutsideClick}>
-      <p>Not Found!</p>
+    <div
+      onClick={handleOutsideClick}
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <NotFoundSVG style={{ width: '50vw' }} />
     </div>
   );
 };
