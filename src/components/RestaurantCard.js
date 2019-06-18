913b8b395b77b04121936e5c09602352b330d682
Neil Berg <neil.berg14@gmail.com>
2019-06-17 20:24:54 -0700

    flex FAB button for centered text

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -36,6 +36,10 @@ const CardContainer = styled(animated.div)`
     color: white;
     font-size: 1.5em;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0;
   }
 
   .card__fab:focus {
